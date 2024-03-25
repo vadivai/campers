fa63036232a5ebee07fcbc58918d5783867fe917
vadivai <vaikanvn@gmail.com>
2024-03-25 21:39:32 +0200

    Add routes, favicon, title, libs

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Routes>
+      <Route path="/" element={<h1>Home Page</h1>}>
+        <Route index element={<h1>Home Page 2</h1>} />
+        <Route path="catalog" element={<h1>Catalog </h1>} />
+        <Route path="catalog/:camperId" element={<h1>Catalog ID </h1>}>
+          <Route path="features" element={<h2>Features </h2>} />
+          <Route path="reviews" element={<h2>Reviews </h2>} />
+        </Route>
+        <Route path="favorites" element={<h2>Favorites </h2>} />
+        <Route path="favorites/:camperId" element={<h2>Favorites ID </h2>}>
+          <Route path="features" element={<h2>Features</h2>} />
+          <Route path="reviews" element={<h2>Reviews </h2>} />
+        </Route>
+        <Route path="*" element={<h2>Another page</h2>} />
+      </Route>
+    </Routes>
+  );
+};
 
-export default App
+export default App;
